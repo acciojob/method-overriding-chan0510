2859f60ed48c5de87de6b1ed85e9a08d6e7a0865
“[Chandan <[chandan.kumar0510@gmail.com]>
2023-03-30 21:07:47 +0530

    Class in Main

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,9 +3,20 @@ package com.driver;
 import org.w3c.dom.ls.LSOutput;
 
 public class Main {
+
     public static void main(String[] args) {
-//        B obj = new B();
-//        System.out.println(obj.meth())
+        B obj = new B();
+        System.out.println(obj.meth());
     }
   
+}
+class B extends A {
+    public String meth() {
+        return "Method is overridden in Extendend class B";
+    }
+}
+class A {
+    public String meth() {
+        return "Invoking method from class A";
+    }
 }
--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -6,15 +6,3 @@ import org.junit.jupiter.api.Test;
 public class TestCases {
     
 }
- class A {
-    public String meth(){
-        return "Invoking method from class A";
-    }
-
-}
-class B extends A {
-    public String meth(){
-        return "Method is overridden in Extendend class B";
-    }
-}
-
