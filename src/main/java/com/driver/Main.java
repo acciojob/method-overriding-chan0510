a0a0dc7a32a208f94409eb50624b5662d0d710b8
“[Chandan <[chandan.kumar0510@gmail.com]>
2023-03-30 20:56:24 +0530

    Assingment completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
+    public static void main(String[] args) {
+        B obj = new B();
+        System.out.println(obj.meth());
+    }
   
 }
