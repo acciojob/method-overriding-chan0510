4b3d6da2969d95988f042d09f233a42a4fed7c15
“[Chandan <[chandan.kumar0510@gmail.com]>
2023-03-30 21:10:16 +0530

    Change Again

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,20 +3,20 @@ package com.driver;
 import org.w3c.dom.ls.LSOutput;
 
 public class Main {
+    static class B extends A {
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+    static class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
 
     public static void main(String[] args) {
         B obj = new B();
         System.out.println(obj.meth());
     }
   
-}
-class B extends A {
-    public String meth() {
-        return "Method is overridden in Extendend class B";
-    }
-}
-class A {
-    public String meth() {
-        return "Invoking method from class A";
-    }
 }
